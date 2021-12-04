2d5a389da7280bcdfe797c4db69b7188cd3cc145
Denny Piza <dpiza@student.42sp.org.br>
2021-12-04 20:00:02 -0300

    adds ft_striteri function

---
+++ src/ft_striteri.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/04 14:09:42 by dpiza             #+#    #+#             */
+/*   Updated: 2021/12/04 17:34:28 by dpiza            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	unsigned int	index;
+
+	if (!s)
+		return ;
+	index = -1;
+	while (s[++index])
+		(*f)(index, &s[index]);
+}
